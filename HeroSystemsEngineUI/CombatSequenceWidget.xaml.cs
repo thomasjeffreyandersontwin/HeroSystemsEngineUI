8f211c09b7114b33063a2be1453ab5074e07f4ef
thomasjeffreyandersontwin <thomasjeffreyandersontwin@gmail.com>
2017-07-12 22:17:32 -0400

    sss
    sss

---
+++ HeroSystemsEngineUI/CombatSequenceWidget.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HeroSystemEngine.Manuevers;
+using HeroSystemsEngine.CombatSequence;
+
+namespace HeroSystemsEngineUI
+{
+    /// <summary>
+    /// Interaction logic for CombatSequenceWidget.xaml
+    /// </summary>
+
+    public partial class CombatSequenceWidget : Page
+    {
+        CombatSequence Sequence;
+      //  public CombatSequence Sequence;
+        public CombatSequenceWidget()
+        {
+            InitializeComponent();
+            CharacterTestObjectFactory Factory=  new CharacterTestObjectFactory();
+
+            CombatSequence sequence = new CombatSequence();
+            sequence.AddCharacter(Factory.BaseCharacter);
+            sequence.AddCharacter(Factory.BaseCharacter);
+            sequence.AddCharacter(Factory.BaseCharacter);
+            CombatSequenceTreeView.ItemsSource = sequence.SegmentList;
+            sequence.StartCombat();
+            Sequence = sequence;
+           // Phase.PhaseStartHandler += RenderSelectedPhase;
+
+
+        }
+
+
+        private void CompletePhaseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Sequence.ActivePhase.Complete();
+        }
+
+
+
+
+        
+    }
+
+}
